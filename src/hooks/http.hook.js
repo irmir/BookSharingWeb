46d3fa37d3bc8aca4a5db1e54c8775d0198475d2
irmir <i.souhat@gmail.com>
2021-03-04 04:59:51 +0300

    restored

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -2,15 +2,13 @@ import { useState, useCallback } from "react"
 
 export const useHttp = () => {
     
-
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
 
     const request = useCallback(async(url, method = 'GET', body = null, headers = {}) => {
-        debugger
         setLoading(true)
         setError(null)
-        
+
         try {
             if (body && body.constructor.name !== "FormData") {
                 body = JSON.stringify(body)
