166fe782f94e4984603a728ae0f02d8fe0a18825
irmir <i.souhat@gmail.com>
2021-03-04 04:48:33 +0300

    added saving profile data to localstor

--- src/redux/userReducer.js
+++ src/redux/userReducer.js
@@ -3,25 +3,41 @@ const initialState = {
     avatar: null,
     email: null,
     phoneNumber: null,
-    userType: null
+    userTypeId: null
 }
 
 export const userReducer = (state = initialState, action) => {
-
+    
     switch(action.type) {
-
+        
         case 'SET_USER_DATA': {
             debugger
+            const profileData = JSON.parse(localStorage.getItem('profileData'))
+
+            if (profileData) {
+
+                return {
+                    ...state,
+                    ...profileData
+                }
+            }
+            
+            debugger
             console.log(action.payload)
-            console.log(action.payload.userType)
-        
-            return {
-                ...state,
+            
+            const newProfileData = {
                 nameUser: action.payload.nickname,
                 avatar: action.payload.avatar,
                 email: action.payload.email,
                 phoneNumber: action.payload.phoneNumber,
-                userType: action.payload.userType                
+                userTypeId: action.payload.userType.id,  
+            }
+
+            localStorage.setItem('profileData', JSON.stringify(newProfileData))
+
+            return {
+                ...state,
+                ...newProfileData           
             }
         }
 
