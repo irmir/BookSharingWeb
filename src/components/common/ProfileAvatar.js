891e9cc7d0c173d93ea24cec215c531538db0617
irmir <i.souhat@gmail.com>
2021-03-31 17:11:17 +0300

    svg fixed

--- src/components/common/ProfileAvatar.js
+++ src/components/common/ProfileAvatar.js
@@ -13,7 +13,7 @@ import ava from '../../image/ava3.jpg'
 const ProfileAvatarComponent = ({ logout, setUserData, profileData, authData }) => {
 
     const { request } = useHttp()
-    debugger
+    
     useEffect(() => {
         if (!profileData) {
             (async function getUserData() {
@@ -41,8 +41,8 @@ const ProfileAvatarComponent = ({ logout, setUserData, profileData, authData }) 
             </div>
             <div onClick={logoutHandler} className="logout">
                 <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11 19l1-1h6V6h-6a1 1 0 010-2h6l2 2v12l-2 2h-6l-1-1z" fill="#fff" fill-opacity=".6" />
-                    <path d="M16 13l-1 1h-5v1l-1 1-5-3v-2l5-3 1 1v1h5l1 1v2z" fill="#fff" fill-opacity=".6" />
+                    <path d="M11 19l1-1h6V6h-6a1 1 0 010-2h6l2 2v12l-2 2h-6l-1-1z" fill="#fff" fillOpacity=".6" />
+                    <path d="M16 13l-1 1h-5v1l-1 1-5-3v-2l5-3 1 1v1h5l1 1v2z" fill="#fff" fillOpacity=".6" />
                 </svg>
             </div>
         </div>
