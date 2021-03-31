6aeb6faf014658a1788c28b2778880f909969ef5
irmir <i.souhat@gmail.com>
2021-03-31 17:06:08 +0300

    pressing the Enter error handling was added, error handling fixed, close window implemented

--- src/components/common/Search.js
+++ src/components/common/Search.js
@@ -1,64 +1,75 @@
-import React, { useEffect } from 'react'
+import React, { useRef, useState } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { useHttp } from '../../hooks/http.hook'
 
-import {Button} from './Button'
+import { Button } from './Button'
 import { SearchCard } from '../cards/SearchCard'
 import { Loader } from '../common/Loader'
+import { Input } from '../common/Input'
 
-import { changeInputValue, showMessage } from '../../redux/siteAction'
-import { getBook } from '../../redux/queryAction'
+import { getBook, hideSearchCard } from '../../redux/queryAction'
 import { Message } from './Message'
 
 
-const SearchComponent = ({changeInputValue, getBook, searchText, searchContent, textMessage}) => {
+const SearchComponent = ({ getBook, searchContent, isSearchCard, hideSearchCard }) => {
+    
+    const { request, loading } = useHttp()
 
-    const {request, error, loading} = useHttp()
+    const searchRef = useRef(null)
 
-    useEffect(() => {
-        if (error) {
-            showMessage(error)
-        }
-    }, [error])
+    const [error, setError] = useState('')
+    const [searchText, setSearchText] = useState(null)
 
-    const changeHandler = (event) => {
-        changeInputValue(event)
+    const onChange = event => {
+        setSearchText(event.target.value)
     }
 
-    const searchHandler = async () => {
-        const data = await request(`/api/books/search/${searchText}`)
-        getBook(data)
+    const searchHandler = async (event) => {
+
+        if (event.type === "keypress" && event.key !== "Enter") {
+            return
+        }
+
+        setError('')
+            try {
+                const data = await request(`/api/books/search/${searchText}`)
+
+                if (data.length === 0) {
+                    setError('Books not found')
+                    hideSearchCard()
+                } else getBook(data)
+            }
+            catch (e) {
+                setError('An error occurred')
+            }
     }
 
     return (
-        <div className="search">
-            <input type="text" placeholder="Find Your Book Here"
-                    onChange={changeHandler} name="searchText" className="search-input"></input>
-        
+        <div ref={searchRef} className="search">
+            <Input type="text" placeholder="Find Your Book Here" value={searchText}
+                onChange={onChange} onKeyPress={searchHandler} name="searchText" className="search-input" />
+
             <Button onClick={searchHandler}
-                    className="parallelogram"
-                    text={<span>Search Book</span>}               
+                className="parallelogram" name="button"
+                text={<span name="button">Search Book</span>}
             />
             {loading && <Loader />}
-            {searchContent && <SearchCard searchContent={searchContent}/>}
+            {isSearchCard && <SearchCard searchContent={searchContent} searchRef={searchRef} />}
             <div className="error">
-                {textMessage && <Message textMessage={textMessage} />}
-            </div>            
+                {error && <Message text={error} />}
+            </div>
         </div>
     )
 }
 
 export const Search = connect(
     (state) => ({
-        searchText: state.site.searchText,
         searchContent: state.query.searchContent,
-        textMessage: state.site.textMessage
+        isSearchCard: state.query.isSearchCard
     }),
     (dispatch) => bindActionCreators({
-        changeInputValue,
         getBook,
-        showMessage 
+        hideSearchCard,
     }, dispatch)
 )(SearchComponent)
-    
