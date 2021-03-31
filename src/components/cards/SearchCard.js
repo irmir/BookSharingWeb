3216c4e47f141d72b641d8050dbb2ab5db5f7142
irmir <i.souhat@gmail.com>
2021-03-31 17:40:24 +0300

    pressing the Enter error handling was added, close window implemented

--- src/components/cards/SearchCard.js
+++ src/components/cards/SearchCard.js
@@ -1,18 +1,47 @@
-import React from 'react'
+import React, { useRef, useEffect, useCallback } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
-import {Book} from '../common/Book'
+import { Book } from '../common/Book'
 
-export const SearchCard = ({ searchContent }) => {
+import { hideSearchCard } from '../../redux/queryAction'
+
+const SearchCardComponent = ({ searchContent, hideSearchCard, searchRef }) => {
+
+    const searchCardRef = useRef(null)
+
+    const clickOutsideHandler = useCallback((event) => {
+
+        if ((event.target !== (searchCardRef.current || searchRef.current)) && !searchCardRef.current.contains(event.target) && !searchRef.current.contains(event.target)) {
+            hideSearchCard()
+        }
+        
+    }, [hideSearchCard])
+
+    useEffect(() => {
+        window.addEventListener('click', clickOutsideHandler, true)
+
+        return () => {
+            window.removeEventListener('click', clickOutsideHandler, true)
+        }
+    }, [clickOutsideHandler])
 
     return (
-        <div className="search-card">
+        <div ref={searchCardRef} className="search-card">
             {
                 searchContent.map((book) => (
                     <div className="book" key={book.id}>
-                            <Book book={book} isRating={true} />
+                        <Book book={book} isRating={true} />
                     </div>
                 ))
             }
         </div>
     )
 }
+
+export const SearchCard = connect(
+    null,
+    (dispatch) => bindActionCreators({
+        hideSearchCard
+    }, dispatch)
+)(SearchCardComponent)
